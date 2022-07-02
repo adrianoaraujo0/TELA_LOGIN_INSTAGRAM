21bd3de52522c5df663feb2f25eab31364eae643
adrianoaraujo0 <adriano.amedeiros@hotmail.com>
2022-07-01 22:19:02 -0300

    Falta implementar a cor do textField e a funcao do botao entrar

---
+++ android/app/src/main/kotlin/com/example/tela_de_login_do_instagram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tela_de_login_do_instagram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  
+  runApp();
+  
+}
+
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
